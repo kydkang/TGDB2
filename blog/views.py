164a0a2ad4c69fe33fc641698c07beabb2408357
KYD <kydkang@gmail.com>
2018-05-31 13:55:15 +0700

    After chapter 9

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 import datetime
+from django.shortcuts import render 
 
 def index(request):
     return HttpResponse("Hello Django")
@@ -8,4 +9,9 @@ def today_is(request):
     now = datetime.datetime.now()
     html = "<html><body>Current date and time: {0}</body></html>".format(now)
     return HttpResponse(html)
+
+def today_is(request):
+    now = datetime.datetime.now()
+    return render(request, 'blog/datetime.html', {'now': now}) 
+
 
