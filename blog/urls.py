9e8ef41db96185ae8e64669167139b8c99ad2f09
Your Name <you@example.com>
2018-06-01 12:57:16 +0700

    After Chater 15

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('time/', views.today_is, name='todays_time'), 
-    path('', views.index, name='blog_index'),   
+    path('category/<slug:category_slug>/', views.post_by_category, name='post_by_category'),
+    path('tag/<slug:tag_slug>/', views.post_by_tag, name='post_by_tag'),
+
+    path('<int:pk>/', views.post_detail, name='post_detail'),
+    path('', views.post_list, name='post_list'),
 ]
