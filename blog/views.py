d5b61f24a853a9e4d9a0c2506858418dc7920d99
KYD <kydkang@gmail.com>
2018-06-01 09:43:11 +0700

    Modified today_is()

--- blog/views.py
+++ blog/views.py
@@ -7,11 +7,6 @@ def index(request):
 
 def today_is(request):
     now = datetime.datetime.now()
-    html = "<html><body>Current date and time: {0}</body></html>".format(now)
-    return HttpResponse(html)
-
-def today_is(request):
-    now = datetime.datetime.now()
     return render(request, 'blog/datetime.html', {'now': now}) 
 
 
