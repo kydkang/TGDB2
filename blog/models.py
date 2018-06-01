3bbe8e6387d0a513e7ca377a7d15f94c865d69cb
KYD <kydkang@gmail.com>
2018-06-01 08:21:21 +0700

    After Chapter 13

--- blog/models.py
+++ blog/models.py
@@ -6,16 +6,23 @@ class Author(models.Model):
     active = models.BooleanField(default=False)
     created_on = models.DateTimeField()
     last_logged_in = models.DateTimeField()
+    def __str__(self):
+        return self.name + " : " + self.email
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
 
 class Tag(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -25,5 +32,7 @@ class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
+    def __str__(self):
+        return self.title
 
 
