dea22e5b5d1b7f4f9884bd7c05b3615e393c3f64
KYD <kydkang@gmail.com>
2018-06-06 10:20:35 +0700

    After Chapter 26

--- blog/views.py
+++ blog/views.py
@@ -5,12 +5,15 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404, redirec
 from django.contrib import messages
 from .forms import FeedbackForm
 from django.core.mail import mail_admins
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django_project import helpers
 
 def index(request):
     return HttpResponse("Hello Django")
 
 def post_list(request):
     posts = Post.objects.order_by("-id").all()
+    posts = helpers.pg_records(request, posts, 5)
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk, post_slug):    
@@ -20,6 +23,7 @@ def post_detail(request, pk, post_slug):
 def post_by_category(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug)
     posts = get_list_or_404(Post.objects.order_by("-id"), category=category)
+    posts = helpers.pg_records(request, posts, 5)
     context = {
         'category': category,
         'posts': posts
@@ -29,6 +33,7 @@ def post_by_category(request, category_slug):
 def post_by_tag(request, tag_slug):
     tag = get_object_or_404(Tag, slug=tag_slug)
     posts = get_list_or_404(Post.objects.order_by("-id"), tags=tag)
+    posts = helpers.pg_records(request, posts, 5)
     context = {
         'tag': tag,
         'posts': posts
