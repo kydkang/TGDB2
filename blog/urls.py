13f90e4225f81e602e38c4b7347d2477a02e2e5f
KYD <kydkang@gmail.com>
2018-06-06 14:02:45 +0700

    After Chapter 28

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
+    path('lousy-login/', views.lousy_login, name='lousy_login'),
+    path('lousy-secret/', views.lousy_secret, name='lousy_secret'),
+    path('lousy-logout/', views.lousy_logout, name='lousy_logout'),
+
+    path('save-session-data/', views.save_session_data, name='save_session_data'),
+    path('access-session-data/', views.access_session_data,name='access_session_data'),
+    path('delete-session-data/', views.delete_session_data, name='delete_session_data'),
+
     path('stop-tracking/', views.stop_tracking, name='stop_tracking'), 
     path('track_user/', views.track_user, name='track_user'), 
     path('cookie/', views.test_cookie, name='cookie'), 
